a05a8873023f19ae6eb7203975a4d59dbb5578c7
Thiago Anderson <thiago.andersom.silva@hotmail.com>
2026-02-25 23:28:32 -0300

    Tests de persistencia com JPA

---
+++ springboot2-essencials/src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,96 @@
+package academy.devdojo.springboot2.repository;
+
+import academy.devdojo.springboot2.domain.Anime;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@DataJpaTest
+@DisplayName("Tests for Anime repository")
+class AnimeRepositoryTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    private Anime createAnime(){
+        return Anime.builder()
+                .name("Hajime no ippo")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Save persists anime when successful")
+    void save_PersistAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    @Test
+    @DisplayName("Save updates anime when successful")
+    void save_UpdatesAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        animeSaved.setName("Overlord");
+        Anime animeUpdated = this.animeRepository.save(animeSaved);
+
+        Assertions.assertThat(animeUpdated).isNotNull();
+        Assertions.assertThat(animeUpdated.getId()).isNotNull();
+        Assertions.assertThat(animeUpdated.getName()).isEqualTo(animeSaved.getName());
+    }
+
+    @Test
+    @DisplayName("Delete removes anime when successful")
+    void delete_Anime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        this.animeRepository.delete(animeSaved);
+
+        Optional<Anime> animeOptional = this.animeRepository.findById(animeSaved.getId());
+
+        Assertions.assertThat(animeOptional.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Find by name returns list of anime when successful")
+    void findByName_Anime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        List<Anime> listAnime = this.animeRepository.findByName(animeSaved.getName());
+
+        Assertions.assertThat(listAnime).isNotEmpty();
+        Assertions.assertThat(listAnime).contains(animeSaved);
+
+    }
+    @Test
+    @DisplayName("Find by name returns empty list of anime when no anime is found")
+    void findByName_ReturnsEmptyListOfAnime_WhenAnimeIsNotFound(){
+
+        List<Anime> listAnime = this.animeRepository.findByName("test value random");
+
+        Assertions.assertThat(listAnime).isEmpty();
+
+    }
+
+    @Test
+    @DisplayName("Save throw ConstraintViolationException when name is empty")
+    void save_ThrowConstraintViolationException_WhenNameIsEmpty(){
+        Anime anime = new Anime();
+        Assertions.assertThatThrownBy(() ->this.animeRepository.save(anime))
+                .isInstanceOf(ConstraintViolationException.class);
+
+
+    }
+
+}
